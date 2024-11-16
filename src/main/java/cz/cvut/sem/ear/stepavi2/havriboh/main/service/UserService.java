30604a93ebffc6532b914a9d5565caca524d4bee
Vitalii Stepaniuk <stepavi2@fel.cvut.cz>
2024-11-16 18:34:10 +0100

    user business logic improved

--- src/main/java/cz/cvut/sem/ear/stepavi2/havriboh/main/service/UserService.java
+++ src/main/java/cz/cvut/sem/ear/stepavi2/havriboh/main/service/UserService.java
@@ -5,6 +5,8 @@ import cz.cvut.sem.ear.stepavi2.havriboh.main.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 public class UserService {
     private final UserDao userDao;
@@ -16,17 +18,25 @@ public class UserService {
     }
 
 
-    public void activateSubscription(User user) {
+    public void activateSubscriptionForOneMonth(User user) {
+        user.setSubscriptionStartDate(LocalDate.now());
+        user.setSubscriptionEndDate(LocalDate.now().plusMonths(1));
         user.setSubscribed(true);
         userDao.update(user);
     }
 
     public void cancelSubscription(User user) {
+        user.setSubscriptionEndDate(LocalDate.now());
         user.setSubscribed(false);
         userDao.update(user);
     }
 
     public boolean isSubscribed(User user) {
-        return user.isSubscribed();
+        // if subscription end date is today or later than today = user is not subscribed
+        if (user.getSubscriptionEndDate().isAfter(LocalDate.now())) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
