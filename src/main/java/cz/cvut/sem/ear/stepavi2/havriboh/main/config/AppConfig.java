35c30b64a8813a1a6c0740bdf14862c3c975ea98
Vitalii Stepaniuk <stepavi2@fel.cvut.cz>
2024-12-03 12:21:27 +0100

    security layer

---
+++ src/main/java/cz/cvut/sem/ear/stepavi2/havriboh/main/config/AppConfig.java
@@ -0,0 +1,15 @@
+package cz.cvut.sem.ear.stepavi2.havriboh.main.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
