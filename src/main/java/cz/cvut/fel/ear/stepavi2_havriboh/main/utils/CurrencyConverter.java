527c44968ad0c3bd486247d54da2790ef13d5398
Vitalii Stepaniuk <stepavi2@fel.cvut.cz>
2024-12-24 18:39:34 +0100

    CurrencyConverter

---
+++ src/main/java/cz/cvut/fel/ear/stepavi2_havriboh/main/utils/CurrencyConverter.java
@@ -0,0 +1,40 @@
+package cz.cvut.fel.ear.stepavi2_havriboh.main.utils;
+
+import cz.cvut.fel.ear.stepavi2_havriboh.main.exception.UnsupportedCurrencyException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+
+public class CurrencyConverter {
+    public static BigDecimal convert(BigDecimal amount, String fromCurrency, String toCurrency) {
+        if (fromCurrency.equals(toCurrency)) {
+            return amount;
+        }
+
+        BigDecimal exchangeRate = getExchangeRate(fromCurrency, toCurrency);
+        return amount.multiply(exchangeRate);
+    }
+
+    private static BigDecimal getExchangeRate(String fromCurrency, String toCurrency) {
+        // Example: EUR -> USD = 1.1, USD -> EUR = 0.9, etc.
+        if (fromCurrency.equals("EUR") && toCurrency.equals("USD")) {
+            return new BigDecimal("1.1");
+        }
+        if (fromCurrency.equals("USD") && toCurrency.equals("EUR")) {
+            return new BigDecimal("0.9");
+        }
+        if (fromCurrency.equals("EUR") && toCurrency.equals("CZK")) {
+            return new BigDecimal("25.5");
+        }
+        if (fromCurrency.equals("CZK") && toCurrency.equals("EUR")) {
+            return new BigDecimal("0.039");
+        }
+        if (fromCurrency.equals("USD") && toCurrency.equals("CZK")) {
+            return new BigDecimal("23.2");
+        }
+        if (fromCurrency.equals("CZK") && toCurrency.equals("USD")) {
+            return new BigDecimal("0.043");
+        }
+        throw new UnsupportedCurrencyException("Unsupported currency pair: " + fromCurrency + " -> " + toCurrency);
+    }
+}
